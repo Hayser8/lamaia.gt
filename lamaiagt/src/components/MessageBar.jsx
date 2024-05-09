50f1b8f11c2af2ed9a8d31098d2e66fdbb3e76c6
Hayser8 <gar22076@uvg.edu.gt>
2024-05-09 00:58:14 -0600

    se eliminó la idea del carrousel y se implementó mejor un banner y se agregó una messagebar

--- lamaiagt/src/components/MessageBar.jsx
+++ lamaiagt/src/components/MessageBar.jsx
@@ -20,8 +20,8 @@ const MessageBar = () => {
   }, []);
 
   return (
-    <div className="fixed top-16 inset-x-0 bg-blue-200 text-white px-4 py-2 text-center">
-      <span className={`transition-opacity duration-500 ${textVisible ? 'opacity-100' : 'opacity-0'}`}>
+    <div className=" bg-blue-200 text-white px-4 py-2 text-center">
+      <span className={` mt-[12px] transition-opacity duration-500 ${textVisible ? 'opacity-100' : 'opacity-0'}`}>
         {messages[messageIndex]}
       </span>
     </div>
